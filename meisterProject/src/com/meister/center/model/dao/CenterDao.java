e63bb598a9f24b45b34b3945dfec1dc70bc9a25a
JISU <63631066+JISUUCHOI@users.noreply.github.com>
2020-04-21 02:19:09 +0900

    1:1문의 작성폼 작업중

--- meisterProject/src/com/meister/center/model/dao/CenterDao.java
+++ meisterProject/src/com/meister/center/model/dao/CenterDao.java
@@ -4,10 +4,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import com.meister.center.model.vo.Center;
 import com.meister.center.model.vo.CenterImage;
+import static com.meister.common.JDBCTemplate.*;
 
 
 public class CenterDao {
@@ -30,6 +32,25 @@ public class CenterDao {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql = prop.getProperty("insertCenter");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, c.getInquiryTitle());
+			pstmt.setString(2, c.getInquiryContent());
+			pstmt.setInt(3, c.getInquiryType());
+			pstmt.setString(4, c.getInquiryStore());
+			pstmt.setInt(5, c.getMemberNo());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
 	}
 	
 	
@@ -37,6 +58,23 @@ public class CenterDao {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql = prop.getProperty("insertCenterImage");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, ci.getOriginName());
+			pstmt.setString(2, ci.getChangeName());
+			pstmt.setString(3, ci.getFilePath());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
 	}
 
 }
