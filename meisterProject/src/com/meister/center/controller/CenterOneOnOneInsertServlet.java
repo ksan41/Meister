a65be305197a617a7ab2c1d4acff1f5ba5bc3a7a
JISU <63631066+JISUUCHOI@users.noreply.github.com>
2020-04-21 01:27:06 +0900

    1:1문의 등록폼 사용중

--- meisterProject/src/com/meister/center/controller/CenterOneOnOneInsertServlet.java
+++ meisterProject/src/com/meister/center/controller/CenterOneOnOneInsertServlet.java
@@ -1,11 +1,17 @@
 package com.meister.center.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 /**
  * Servlet implementation class CenterOneOnOneInsertServlet
@@ -26,8 +32,23 @@ public class CenterOneOnOneInsertServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		request.setCharacterEncoding("utf-8");
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			
+			// 1. 전송된 파일을 위해 처리할 작업
+			int maxSize = 10 * 1024 * 1024;
+			
+			String resources = request.getSession().getServletContext().getRealPath("/resources");
+			String savePath = resources + "\\center_upfiles\\";
+			
+			// 2. 전달된 파일명 수정 및 업로드 작업
+			MultipartRequest multiRequest 
+				= new MultipartRequest(request, savePath, maxSize, "utf-8", new DefaultFileRenamePolicy());
+		
+		}
+	
 	}
 
 	/**
