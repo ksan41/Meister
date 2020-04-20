8f681096cd98fd2f385429a4aa34e130a7a3e943
JISU <63631066+JISUUCHOI@users.noreply.github.com>
2020-04-20 21:04:09 +0900

    1:1문의 리스트 수정중

--- meisterProject/src/com/meister/myPage/controller/MyOneListServlet.java
+++ meisterProject/src/com/meister/myPage/controller/MyOneListServlet.java
@@ -1,11 +1,18 @@
 package com.meister.myPage.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.center.model.vo.Center;
+import com.meister.myPage.model.service.MyPageService;
+import com.meister.myPage.model.vo.Member;
 
 /**
  * Servlet implementation class MyOneListServlet
@@ -26,8 +33,17 @@ public class MyOneListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		int memberNo = ((Member)request.getSession().getAttribute("loginUser")).getMemberNo();
+		
+		// 해당 회원이 작성한 1:1문의(CENTER 테이블) 리스트 조회 후 응답페이지에 전달
+		ArrayList<Center> list = new MyPageService().selectCenterList(memberNo);
+		
+		request.setAttribute("list", list);
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/user/myPage/myPageOneOnOneList.jsp");
+		view.forward(request, response);
+		
 	}
 
 	/**
