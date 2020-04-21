6d7a67bd7c6245bdb30cdbd10ffac348852fa346
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-04-21 21:25:51 +0900

    관리자 공지사항 서블릿 정리

--- meisterProject/src/com/meister/notice/controller/NoticeMgBmDetailServlet.java
+++ meisterProject/src/com/meister/notice/controller/NoticeMgBmDetailServlet.java
@@ -1,7 +1,6 @@
 package com.meister.notice.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,18 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 import com.meister.notice.model.service.NoticeService;
 import com.meister.notice.model.vo.Notice;
 
-//////통합관리자 고객공지사항 리스트
+
+/// 통합관리자 지점공지사항 관리용 상세조회
 /**
- * Servlet implementation class NoticeListServlet
+ * Servlet implementation class NoticeDetailServlet
  */
-@WebServlet("/imNoticeMlist.nom")
-public class NoticeMgListServlet extends HttpServlet {
+@WebServlet("/imNoticeBdetail.nom")
+public class NoticeMgBmDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NoticeMgListServlet() {
+    public NoticeMgBmDetailServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,12 +34,26 @@ public class NoticeMgListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		ArrayList<Notice> list = new NoticeService().selectList();
+		int nno = Integer.parseInt(request.getParameter("nno"));
 		
-		request.setAttribute("list", list);
+		Notice n = new NoticeService().selectNotice(nno);
 		
-		RequestDispatcher view = request.getRequestDispatcher("views/manager/noticeMg/noticeMgList.jsp");
-		view.forward(request, response);
+		if(n != null) {	// 조회성공
+			
+			new NoticeService().increaseCount(nno);
+
+			request.setAttribute("n", n);
+			RequestDispatcher view = request.getRequestDispatcher("views/manager/noticeMg/noticeMgDetail.jsp");
+			view.forward(request, response);
+			
+		}else {			// 조회실패
+			
+		}
+		
+		
+
+		
+		
 	}
 
 	/**
