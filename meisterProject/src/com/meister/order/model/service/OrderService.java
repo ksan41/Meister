ff7fe2424bd2ec03d6acda25a1e8e9769b4fe8ed
JinAh <58837220+saySthAbout@users.noreply.github.com>
2020-04-21 20:05:16 +0900

    OrderService 생성

--- meisterProject/src/com/meister/order/model/service/OrderService.java
+++ meisterProject/src/com/meister/order/model/service/OrderService.java
@@ -1,5 +1,29 @@
 package com.meister.order.model.service;
 
+import static com.meister.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import com.meister.order.model.dao.OrderDao;
+import com.meister.order.model.vo.Delivery;
+
 public class OrderService {
+	public int insertAddress(Delivery b) {
+		
+		Connection conn = getConnection();
+		
+		int result = new OrderDao().insertAddress(conn, b);
+		
+		if(result > 0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
+	}
+	
 
 }
