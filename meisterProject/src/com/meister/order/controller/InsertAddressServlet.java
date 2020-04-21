2aa1c07205ada0d308cf97d12a6bfb39985b6b13
JinAh <58837220+saySthAbout@users.noreply.github.com>
2020-04-21 11:16:29 +0900

    Delivery vo객체 생성

--- meisterProject/src/com/meister/order/controller/InsertAddressServlet.java
+++ meisterProject/src/com/meister/order/controller/InsertAddressServlet.java
@@ -21,7 +21,7 @@ public class InsertAddressServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+ 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
