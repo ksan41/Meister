aa03af4128c563c1f663d05851b8b4ddbac463ff
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-04-22 15:57:31 +0900

    MemberMgDeleteMemberServlet 작업

--- meisterProject/src/com/meister/member/controller/MemberMgDeleteMemberServlet.java
+++ meisterProject/src/com/meister/member/controller/MemberMgDeleteMemberServlet.java
@@ -1,11 +1,16 @@
 package com.meister.member.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.member.model.service.MemberService;
+import com.meister.member.model.vo.Member;
 
 /**
  * Servlet implementation class MemberMgDeleteMemberServlet
@@ -26,8 +31,27 @@ public class MemberMgDeleteMemberServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int memberNo = Integer.parseInt(request.getParameter("memNo"));
+		
+		Member m = new Member();
+		m.setMemberNo(memberNo);
+		
+		int result = new MemberService().deleteMember(m);
+		
+		if(result>0) {//회원탈퇴처리 성공시
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('해당 회원이 탈퇴되었습니다.');location.href='/Meister/memberList.memg';</script>");
+			out.flush();
+		}else { //회원탈퇴처리 실패시
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('회원탈퇴처리 실패. 다시 시도해주세요.');location.href='/Meister/memberList.memg';</script>");
+			out.flush();
+		}
+			
+		
 	}
 
 	/**
