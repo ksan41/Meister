b145849cc66422365473ea68990f66a306529873
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-04-22 21:58:31 +0900

    MemberMgListServlet 작업

--- meisterProject/src/com/meister/member/controller/MemberMgListServlet.java
+++ meisterProject/src/com/meister/member/controller/MemberMgListServlet.java
@@ -1,6 +1,8 @@
 package com.meister.member.controller;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -12,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.meister.member.model.service.MemberService;
 import com.meister.member.model.vo.Member;
-
+import static com.meister.common.StringToDate.*;
 
 /**
  * Servlet implementation class MemberMgListServlet
@@ -20,40 +22,63 @@ import com.meister.member.model.vo.Member;
 @WebServlet("/memberList.memg")
 public class MemberMgListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberMgListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		ArrayList<Member> list = new MemberService().selectMemberList();
-		
-		request.setAttribute("list", list);
-		
-		if(list.isEmpty()) { //조회된 회원정보가 없을 경우
-			request.setAttribute("msg", "조회된 결과가 없습니다.");
-			RequestDispatcher view = request.getRequestDispatcher("views/manager/memberMg/memberMgList.jsp");
-			view.forward(request, response);
-		}else { // 조회된 회원정보가 있을 경우
-			RequestDispatcher view = request.getRequestDispatcher("views/manager/memberMg/memberMgList.jsp");
-			view.forward(request, response);			
-		}
-		
-		
+	public MemberMgListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String name = null; 
+		name= request.getParameter("userName");
+		
+		Date startDate=null;
+		Date endDate=null;
+//		startDate = stringToDate(request.getParameter("startDate"));
+//		endDate = stringToDate(request.getParameter("endDate"));
+
+		if (name!=null) {// 이름으로 검색시
+			ArrayList<Member> list = new MemberService().searchName(name);
+			
+			request.setAttribute("list", list);
+			RequestDispatcher view = request.getRequestDispatcher("views/manager/memberMg/memberMgList.jsp");
+			view.forward(request, response);
+//		} else if (startDate != null || endDate != null) { // 기간별 검색시
+//
+//			ArrayList<Member> list = new MemberService().searchDate(startDate, endDate);
+//
+//			RequestDispatcher view = request.getRequestDispatcher("views/manager/memberMg/memberMgList.jsp");
+//			view.forward(request, response);
+
+		} else {// 기본 전체조회용
+			ArrayList<Member> list = new MemberService().selectMemberList();
+
+			request.setAttribute("list", list);
+			RequestDispatcher view = request.getRequestDispatcher("views/manager/memberMg/memberMgList.jsp");
+			view.forward(request, response);
+
+
+		}
+
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
