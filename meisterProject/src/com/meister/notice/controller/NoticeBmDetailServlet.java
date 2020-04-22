6fc413cceaefef343bc5f7f47ee67c6fe9271ef9
YEONHWAJO <nacho3500@gmail.com>
2020-04-22 13:57:08 +0900

    지점관리자-지점공지

--- meisterProject/src/com/meister/notice/controller/NoticeBmDetailServlet.java
+++ meisterProject/src/com/meister/notice/controller/NoticeBmDetailServlet.java
@@ -1,11 +1,16 @@
 package com.meister.notice.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.notice.model.service.NoticeService;
+import com.meister.notice.model.vo.Notice;
 
 
 //// 지점관리자 페이지 지점공지사항 상세페이지(조회용)
@@ -29,7 +34,23 @@ public class NoticeBmDetailServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int nno = Integer.parseInt(request.getParameter("nno"));
+		
+		Notice n = new NoticeService().selectNotice(nno);
+		
+		if(n != null) {	// 조회성공
+			
+			new NoticeService().increaseCount(nno);
+
+			request.setAttribute("n", n);
+			RequestDispatcher view = request.getRequestDispatcher("views/manager/noticeMg/bmBranchNoticeDetail.jsp");
+			view.forward(request, response);
+			
+		}else {			// 조회실패
+			
+		}
 	}
 
 	/**
--- meisterProject/src/com/meister/notice/controller/NoticeDetailServlet.java
+++ meisterProject/src/com/meister/notice/controller/NoticeDetailServlet.java
@@ -48,10 +48,6 @@ public class NoticeDetailServlet extends HttpServlet {
 			
 		}
 		
-		
-
-		
-		
 	}
 
 	/**
