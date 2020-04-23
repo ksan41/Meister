f28d3a3a430101379b1f374fba9348e9cbd10543
YEONHWAJO <nacho3500@gmail.com>
2020-04-23 11:45:42 +0900

    고객공지등록서블릿

--- meisterProject/src/com/meister/notice/controller/NoticeMgEnrollFormServlet.java
+++ meisterProject/src/com/meister/notice/controller/NoticeMgEnrollFormServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class NoticeMgEnrollFormServlet
  */
-@WebServlet("/NoticeMgEnrollFormServlet")
+@WebServlet("/imNoticeMenroll.nom")
 public class NoticeMgEnrollFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
