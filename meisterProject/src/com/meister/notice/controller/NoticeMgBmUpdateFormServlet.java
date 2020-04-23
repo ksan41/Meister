8a35e6b4178b848e98527ce81b0571a33cfd24b6
YEONHWAJO <nacho3500@gmail.com>
2020-04-23 20:51:59 +0900

    Update NoticeMgBmUpdateFormServlet.java

--- meisterProject/src/com/meister/notice/controller/NoticeMgBmUpdateFormServlet.java
+++ meisterProject/src/com/meister/notice/controller/NoticeMgBmUpdateFormServlet.java
@@ -1,11 +1,16 @@
 package com.meister.notice.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.notice.model.service.NoticeService;
+import com.meister.notice.model.vo.Notice;
 
 /**
  * Servlet implementation class NoticeMgBmUpdateFormServlet
@@ -27,7 +32,24 @@ public class NoticeMgBmUpdateFormServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		int nno = Integer.parseInt(request.getParameter("nno"));	// "42"
 		
+		Notice n = new NoticeService().selectNotice(nno);
+		
+		if(n != null) {
+			
+			request.setAttribute("n", n);
+			request.getRequestDispatcher("views/manager/noticeMg/branchNoticeUpdateForm.jsp").forward(request, response);
+			
+		}else {
+			
+			response.setContentType("text/html; charset=UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("<script>alert('지점 공지사항 수정폼이 실패했습니다. 다시해!!!!'); location.href='/Meister/imNoticeBdetail.nom?nno="+nno+"';</script>");
+			out.flush();
+		}
 	}
 
 	/**
