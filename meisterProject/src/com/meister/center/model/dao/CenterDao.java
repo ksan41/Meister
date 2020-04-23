8eb506cd353b3bf5a0f4931c430fe411a24a41c9
xorud3290 <xorud3290@naver.com>
2020-04-23 16:25:59 +0900

    faq

--- meisterProject/src/com/meister/center/model/dao/CenterDao.java
+++ meisterProject/src/com/meister/center/model/dao/CenterDao.java
@@ -4,11 +4,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import com.meister.center.model.vo.Center;
 import com.meister.center.model.vo.CenterImage;
+import com.meister.center.model.vo.Faq;
+
 import static com.meister.common.JDBCTemplate.*;
 
 
@@ -54,6 +59,8 @@ public class CenterDao {
 	}
 	
 	
+	
+	
 	public int insertInquiryImage(Connection conn, CenterImage ci) {
 		int result = 0;
 		PreparedStatement pstmt = null;
@@ -76,5 +83,51 @@ public class CenterDao {
 		
 		return result;
 	}
+	
+	
+	
+	
+	public ArrayList<Faq> selectList(Connection conn) {
+		
+		ArrayList<Faq> list = new ArrayList<>();
+		
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("selectList");
+		
+		try {
+			
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(sql);
+			
+			while(rset.next()) {
+				
+			    list.add(new Faq(rset.getInt("FAQ_NO"),
+			    		            rset.getString("FAQ_TYPE"),
+			    		            rset.getString("FAQ_QUESTION"),
+			    		            rset.getString("FAQ_ANSWER")));
+			    
+									
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(stmt);
+		}
+		
+		return list;
+		
+		
+		
+		
+	}
+	
+	
+   
 
 }
