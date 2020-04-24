dcf4548d9e6652b409c472d79c7659c1c83ea4b0
YEONHWAJO <nacho3500@gmail.com>
2020-04-24 19:17:57 +0900

    Update CenterMgFaqUpdateServlet.java

--- meisterProject/src/com/meister/center/controller/CenterMgFaqUpdateServlet.java
+++ meisterProject/src/com/meister/center/controller/CenterMgFaqUpdateServlet.java
@@ -1,11 +1,16 @@
 package com.meister.center.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.notice.model.service.NoticeService;
+import com.meister.notice.model.vo.Notice;
 
 /**
  * Servlet implementation class CenterMgFaqUpdateServlet
@@ -27,7 +32,34 @@ public class CenterMgFaqUpdateServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// 요청시 한글이 전달될 경우를 대비해서
+		request.setCharacterEncoding("utf-8");
 		
+		int nno = Integer.parseInt(request.getParameter("nno"));
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		Notice n = new Notice();
+		n.setNoticeNo(nno);
+		n.setNoticeTitle(title);
+		n.setNoticeContent(content);
+		
+		int result = new NoticeService().updateNotice(n);
+		
+		if(result > 0) {
+			
+			// 상세보기 요청
+			response.sendRedirect("ceMgFaqList.cem?nno=" + nno);
+			
+		}else {
+			
+			response.setContentType("text/html; charset=UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("<script>alert('고객 공지사항 수정이 실패했습니다. 다시 수정해!!!!'); location.href='/Meister/ceMgFaqList.cem?nno="+nno+"';</script>");
+			out.flush();
+		}
 	}
 
 	/**
