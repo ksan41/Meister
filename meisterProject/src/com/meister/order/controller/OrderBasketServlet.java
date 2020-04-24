80e20dbd1660c14b49a67ee6a1fd00c8ed80591d
HYK331 <63688749+HYK331@users.noreply.github.com>
2020-04-24 21:30:39 +0900

    장바구니다

--- meisterProject/src/com/meister/order/controller/OrderBasketServlet.java
+++ meisterProject/src/com/meister/order/controller/OrderBasketServlet.java
@@ -9,9 +9,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.meister.member.model.vo.Member;
+import com.meister.menu.model.service.MenuService;
+import com.meister.menu.model.vo.Dough;
+import com.meister.menu.model.vo.Etc;
+import com.meister.menu.model.vo.Pizza;
+import com.meister.menu.model.vo.PizzaSize;
+import com.meister.menu.model.vo.Side;
 import com.meister.order.model.service.OrderService;
 import com.meister.order.model.vo.Price;
 
@@ -22,8 +27,6 @@ import com.meister.order.model.vo.Price;
 public class OrderBasketServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	HttpSession session = null;
-	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,18 +39,33 @@ public class OrderBasketServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		session = request.getSession();
-		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		Member loginUser = (Member)request.getSession().getAttribute("loginUser");
 		int userNo = loginUser.getMemberNo();
 		System.out.println("orderBasket 딴에서 userNo = " + userNo);
 		
 		// 3. 해당 요청을 처리하는 서비스클래스의 메소드 호출 후 처리 결과 받기
-		ArrayList<Price> basketList = new OrderService().ShowBasketList(userNo);
+		Price basket = new OrderService().ShowBasketList(userNo);
+		
+		ArrayList<Pizza> pList = new MenuService().selectPizzaList();
+		ArrayList<PizzaSize> sizeList = new MenuService().selectPizzaSizeList();
+		
+		ArrayList<Side> sList = new MenuService().selectSideList();
+		ArrayList<Etc> eList = new MenuService().selectEtcList();
+		ArrayList<Dough> dList = new MenuService().selectDoughList();
+		
+		
 		//System.out.println("서블릿딴 : " +deliveryList.get(0));
 		
 		// 4. 처리 결과를 통해 사용자가 보게될 뷰 요청
-		session = request.getSession();
-		session.setAttribute("basketList", basketList);
+		request.setAttribute("basket", basket);
+		request.setAttribute("pList", pList);
+		request.setAttribute("sizeList", sizeList);
+		request.setAttribute("sList", sList);
+		request.setAttribute("eList", eList);
+		request.setAttribute("dList", dList);
+		
+		System.out.println(basket);
 		RequestDispatcher view = request.getRequestDispatcher("/views/user/order/orderBasketMenu.jsp");
 		view.forward(request, response);
 	}
