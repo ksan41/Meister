77450e12d543e5eaf028fa993e35c10e03b61a6c
YEONHWAJO <nacho3500@gmail.com>
2020-04-26 20:30:55 +0900

    order list servlet doget

--- meisterProject/src/com/meister/order/controller/OrderMgNowListServlet.java
+++ meisterProject/src/com/meister/order/controller/OrderMgNowListServlet.java
@@ -1,11 +1,17 @@
 package com.meister.order.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.order.model.service.OrderService;
+import com.meister.order.model.vo.Orders;
 
 /**
  * Servlet implementation class OrderMgNowListServlet
@@ -27,7 +33,12 @@ public class OrderMgNowListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		ArrayList<Orders> list = new OrderService().selectMgList();
 		
+		request.setAttribute("list", list);
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/manager/orderMg/orderMgNowList.jsp");
+		view.forward(request, response);
 	}
 
 	/**
--- meisterProject/src/com/meister/order/controller/OrderMgPastListServlet.java
+++ meisterProject/src/com/meister/order/controller/OrderMgPastListServlet.java
@@ -1,11 +1,17 @@
 package com.meister.order.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.meister.order.model.service.OrderService;
+import com.meister.order.model.vo.Orders;
 
 /**
  * Servlet implementation class OrderMgPastListServlet
@@ -27,7 +33,12 @@ public class OrderMgPastListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		ArrayList<Orders> list = new OrderService().selectMgList();
 		
+		request.setAttribute("list", list);
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/manager/orderMg/orderMgPastList.jsp");
+		view.forward(request, response);
 	}
 
 	/**
