4707f080da1393b5db63b97b1dc0e47b166448ea
JISU <63631066+JISUUCHOI@users.noreply.github.com>
2020-04-27 21:45:46 +0900

    현영오빠에게 토스~~~^ㅇ^

--- meisterProject/src/com/meister/order/controller/OrderPaymentSuccessServlet.java
+++ meisterProject/src/com/meister/order/controller/OrderPaymentSuccessServlet.java
@@ -1,7 +1,9 @@
 package com.meister.order.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,20 +38,43 @@ public class OrderPaymentSuccessServlet extends HttpServlet {
 		
 		int memberNo = ((Member)request.getSession().getAttribute("loginUser")).getMemberNo();
 		int orderNo = Integer.parseInt(request.getParameter("orderNo"));
+		int cartNo = Integer.parseInt(request.getParameter("cartNo"));
+		int deliveryNo = Integer.parseInt(request.getParameter("deliveryNo"));
+		int totalPrice = Integer.parseInt(request.getParameter("totalPrice"));
+		
 		
 		String name = request.getParameter("name");
 		String phone = request.getParameter("phone1") + "-" + request.getParameter("phone2") + "-" + request.getParameter("phone3");
 		String requirements = request.getParameter("requirements");
 		
 		Orders newOrder = new Orders();
-		newOrder.setMemberNo(memberNo);
-		newOrder.setOrderNo(orderNo);
 		newOrder.setOrderName(name);
 		newOrder.setOrderPhone(phone);
 		newOrder.setOrderRequest(requirements);
+		newOrder.setDeliveryNo(deliveryNo);
+		newOrder.setMemberNo(memberNo);
+		newOrder.setCartNo(cartNo);
+		newOrder.setOrderNo(orderNo);
+		newOrder.setTotalPrice(totalPrice);
 		
 		int result = new OrderService().insertOrders(newOrder);
 		
+		if(result > 0) { // 새 Orders insert성공
+			
+			request.setAttribute("totalPrice", totalPrice);
+			
+			RequestDispatcher view = request.getRequestDispatcher("views/user/order/orderPayment.jsp");
+			view.forward(request, response);
+			
+		}else { // 새 Orders insert실패
+			
+			response.setContentType("text/html; charset=UTF-8");
+			 
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('결제에 실패했습니다. 다시 시도해주세요.'); history.back(-1);</script>");
+			out.flush();
+			
+		}
 		
 	}
 
