4462144ecc6552c782456bc98d3bbbe5caca6f71
HYK331 <63688749+HYK331@users.noreply.github.com>
2020-04-29 17:29:05 +0900

    서블릿

---
+++ meisterProject/src/com/meister/order/controller/OrderPaymentSuccessPageServlet.java
@@ -0,0 +1,45 @@
+package com.meister.order.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OrderPaymentSuccessPageServlet
+ */
+@WebServlet("/paymentSuccess.or")
+public class OrderPaymentSuccessPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OrderPaymentSuccessPageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/user/order/orderPaymentSuccess.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
